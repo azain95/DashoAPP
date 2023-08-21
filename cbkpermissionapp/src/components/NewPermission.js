799d1178c3f6f6f8ebf89f1817f611fa83f3c331
Ahmed Zain Al-Hamawi <78156960+azain95@users.noreply.github.com>
2023-08-21 16:49:15 +0300

    restructure

---
+++ cbkpermissionapp/src/components/NewPermission.js
@@ -0,0 +1,172 @@
+// App.js
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, NavLink, Navigate, Switch } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Select, MenuItem, FormControl, InputLabel, TextField, Typography } from '@mui/material';
+import { AppBar, Toolbar, IconButton, Drawer, List, ListItem, ListItemText, useMediaQuery, useTheme, Grid } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Button, Link as MuiLink } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import axios from 'axios';
+import { TextareaAutosize } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { TimePicker } from '@mui/x-date-pickers';
+import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { format } from 'date-fns';
+import { Snackbar } from '@mui/material';
+import Alert from '@mui/material/Alert';
+import { Link as RouterLink } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+function NewPermission() {
+    const [startDate, setStartDate] = useState(new Date());
+    const [startTime, setStartTime] = useState(new Date()); // Initialize with a Date object
+    const [endDate, setEndDate] = useState(new Date());
+    const [endTime, setEndTime] = useState(new Date()); // Initialize with a Date object
+    const [reason, setReason] = useState('');
+    const [user, setUser] = useState(null);
+    const [notification, setNotification] = useState({ open: false, message: '', severity: 'success' });
+    const [reqType, setReqType] = useState('permission'); // New state to store request type
+  
+  
+  
+    useEffect(() => {
+      const userData = Cookies.get('user');
+      if (userData) {
+        setUser(JSON.parse(userData));
+      }
+    }, []);
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      const user = JSON.parse(Cookies.get('user'));
+  
+          // Format the time values
+          const formattedStartTime = format(startTime, 'HH:mm');
+          const formattedEndTime = format(endTime, 'HH:mm');
+  
+              // Check if time is selected
+      if (formattedStartTime === 'Invalid Date' || formattedEndTime === 'Invalid Date') {
+        setNotification({
+          open: true,
+          message: 'Please select a valid start and end time.',
+          severity: 'error',
+        });
+        return;
+      }
+    
+      // Check that the user object exists before attempting to access its properties
+      if (!user) {
+        console.error('User object is undefined');
+        return;
+      }
+    
+      // Log the user object to the console
+      console.log('User object:', user);
+    
+       const permission = {
+        req_datetime: new Date().toISOString(),
+        req_type: reqType, 
+        date_from: startDate.toISOString(),
+        date_to: endDate.toISOString(),
+        time_from: formattedStartTime,
+        time_to: formattedEndTime,
+        user_id: user.user_id, // Now safe to access, as we've checked that user is defined
+        reason: reason,
+        status: 'pending',
+      };
+    
+      // Retrieve the token from cookies
+      const token = Cookies.get('token');
+    
+      try {
+        const response = await axios.post('http://localhost:5000/requests', permission, {
+          headers: {
+            Authorization: `Bearer ${token}` // Including the token in the Authorization header
+          }
+        });
+        // Clear the form fields
+        setStartDate(new Date());
+        setStartTime(new Date());
+        setEndDate(new Date());
+        setEndTime(new Date());
+        setReason('');
+  
+        setNotification({
+          open: true,
+          message: 'Request submitted successfully!',
+          severity: 'success',
+        });
+      } catch (error) {
+        console.error(error);
+        setNotification({
+          open: true,
+          message: 'Error submitting request. Please try again.',
+          severity: 'error',
+        });
+        // Clear the form fields
+  setStartDate(new Date());
+  setStartTime('12:00');
+  setEndDate(new Date());
+  setEndTime('12:00');
+  setReason('');
+      }
+    };
+    const handleCloseNotification = () => {
+      setNotification({ ...notification, open: false });
+    };
+    return (
+      <Container component={Paper} style={{ padding: 20 }}>
+        <h2>New Permission</h2>
+        <form onSubmit={handleSubmit}>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <FormControl variant="outlined" fullWidth style={{ marginBottom: 15 }}>
+              <InputLabel htmlFor="reqType">Request Type</InputLabel>
+              <Select
+                label="Request Type"
+                value={reqType}
+                onChange={(e) => setReqType(e.target.value)}
+              >
+                <MenuItem value="permission">Permission</MenuItem>
+                <MenuItem value="swap">Swap</MenuItem>
+              </Select>
+            </FormControl>
+            <InputLabel htmlFor="startDate">Start Date:</InputLabel>
+            <FormControl fullWidth style={{ marginBottom: 15 }}>
+              <DatePicker selected={startDate} onChange={setStartDate} dateFormat="yyyy-MM-dd" required />
+            </FormControl>
+            <TimePicker
+              label="Start Time"
+              value={startTime}
+              onChange={setStartTime}
+              renderInput={(params) => <TextField {...params} fullWidth required />}
+            />
+            <InputLabel htmlFor="endDate">End Date:</InputLabel>
+            <FormControl fullWidth style={{ marginBottom: 15, marginTop: 15 }}>
+              <DatePicker selected={endDate} onChange={setEndDate} dateFormat="yyyy-MM-dd" required />
+            </FormControl>
+            <TimePicker
+              label="End Time"
+              value={endTime}
+              onChange={setEndTime}
+              renderInput={(params) => <TextField {...params} fullWidth required />}
+            />
+            <TextField label="Reason" value={reason} onChange={(e) => setReason(e.target.value)} fullWidth required multiline rows={4} style={{ marginTop: 15 }} />
+            <Button type="submit" variant="contained" color="primary" style={{ marginTop: 20 }}>Submit</Button>
+          </LocalizationProvider>
+        </form>
+        <Snackbar open={notification.open} autoHideDuration={6000} onClose={handleCloseNotification}>
+          <Alert onClose={handleCloseNotification} severity={notification.severity} sx={{ width: '100%' }}>
+            {notification.message}
+          </Alert>
+        </Snackbar>
+      </Container>
+    );
+  }
+
+  
+  export default NewPermission
---
+++ cbkpermissionapp/src/components/PermissionHistory.js
@@ -0,0 +1,155 @@
+
+// App.js
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, NavLink, Navigate, Switch } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Select, MenuItem, FormControl, InputLabel, TextField, Typography } from '@mui/material';
+import { AppBar, Toolbar, IconButton, Drawer, List, ListItem, ListItemText, useMediaQuery, useTheme, Grid } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Button, Link as MuiLink } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import axios from 'axios';
+import { TextareaAutosize } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { TimePicker } from '@mui/x-date-pickers';
+import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { format } from 'date-fns';
+import { Snackbar } from '@mui/material';
+import Alert from '@mui/material/Alert';
+import { Link as RouterLink } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+function PermissionHistory() {
+    const [permissions, setPermissions] = useState([]);
+    const [filters, setFilters] = useState({
+      type: '',
+      status: '',
+      startDate: null,
+      startTime: '',
+      endDate: null,
+      endTime: '',
+      reason: '',
+      attachment: ''
+    });
+    const theme = useTheme();
+  
+    useEffect(() => {
+      const fetchPermissionHistory = async () => {
+        try {
+          const user = JSON.parse(Cookies.get('user'));
+          if (!user) {
+            console.error('User object is undefined');
+            return;
+          }
+  
+          const token = Cookies.get('token');
+  
+          const response = await axios.get(`http://localhost:5000/requests/user/${user.user_id}`, {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+  
+          if (response.data && response.data.length > 0) {
+            const permissionsData = response.data.filter(req => ['permission', 'swap'].includes(req.req_type.toLowerCase()));
+            permissionsData.sort((a, b) => new Date(b.req_datetime) - new Date(a.req_datetime));
+            setPermissions(permissionsData);
+          } else {
+            setPermissions([]);
+          }
+        } catch (error) {
+          console.error('Error retrieving permission history:', error.message);
+          setPermissions([]);
+        }
+      };
+  
+      fetchPermissionHistory();
+    }, []);
+  
+    const getStatusLabel = (status) => {
+      if (status === 'pending') {
+        return <span className="status-pending">Pending</span>;
+      } else if (status === 'rejected') {
+        return <span className="status-rejected">Rejected</span>;
+      } else {
+        return <span className="status-approved">Approved</span>;
+      }
+    };
+  
+    const formatDate = (dateString) => {
+      const [year, month, day] = dateString.split('T')[0].split('-');
+      return `${day}/${month}/${year}`;
+    };
+  
+    const handleSearchChange = (e, field) => {
+      setFilters({ ...filters, [field]: e.target.value });
+    };
+  
+    const filteredPermissions = permissions.filter((permission) => {
+      if (filters.type && !permission.req_type.includes(filters.type)) return false;
+      if (filters.status && permission.status !== filters.status) return false;
+      if (filters.startDate && formatDate(permission.date_from) !== filters.startDate) return false;
+      if (filters.startTime && !permission.time_from.includes(filters.startTime)) return false;
+      if (filters.endDate && formatDate(permission.date_to) !== filters.endDate) return false;
+      if (filters.endTime && !permission.time_to.includes(filters.endTime)) return false;
+      if (filters.reason && !permission.reason.includes(filters.reason)) return false;
+      if (filters.attachment && !permission.attachment.includes(filters.attachment)) return false;
+      return true;
+    });
+  
+    return (
+      <div className="content">
+        <Typography variant="h4" gutterBottom>Permission History</Typography>
+        <div className="filter-container">
+          <TextField label="Type" variant="outlined" size="small" onChange={(e) => handleSearchChange(e, 'type')} />
+          <TextField label="Status" variant="outlined" size="small" onChange={(e) => handleSearchChange(e, 'status')} />
+          <TextField label="Start Date (DD/MM/YYYY)" variant="outlined" size="small" onChange={(e) => handleSearchChange(e, 'startDate')} />
+          <TextField label="Start Time" variant="outlined" size="small" onChange={(e) => handleSearchChange(e, 'startTime')} />
+          <TextField label="End Date (DD/MM/YYYY)" variant="outlined" size="small" onChange={(e) => handleSearchChange(e, 'endDate')} />
+          <TextField label="End Time" variant="outlined" size="small" onChange={(e) => handleSearchChange(e, 'endTime')} />
+          <TextField label="Reason" variant="outlined" size="small" onChange={(e) => handleSearchChange(e, 'reason')} />
+        </div>
+        <Paper style={{ marginTop: '20px' }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Type</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell>Start Date</TableCell>
+                <TableCell>Start Time</TableCell>
+                <TableCell>End Date</TableCell>
+                <TableCell>End Time</TableCell>
+                <TableCell>Reason</TableCell>
+                {/* <TableCell>Attachment</TableCell> */}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {filteredPermissions.length > 0 ? (
+                filteredPermissions.map((permission) => (
+                  <TableRow key={permission.id}>
+                    <TableCell>{permission.req_type}</TableCell>
+                    <TableCell>{getStatusLabel(permission.status)}</TableCell>
+                    <TableCell>{formatDate(permission.date_from)}</TableCell>
+                    <TableCell>{permission.time_from}</TableCell>
+                    <TableCell>{formatDate(permission.date_to)}</TableCell>
+                    <TableCell>{permission.time_to}</TableCell>
+                    <TableCell>{permission.reason}</TableCell>
+                    {/* <TableCell>{permission.attachment}</TableCell> */}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                <TableCell colSpan="8" align="center">No permissions found.</TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </Paper>
+    </div>
+  );
+  }
+export default PermissionHistory  
